f3be9b5fb55db21d41e576519adbf0170fbb5cdb
Abhay suresh wadkar <154788447+Abhaywadkar04@users.noreply.github.com>
2024-08-07 20:08:05 +0530

    Time: 0 ms (100%), Space: 11.3 MB (61.34%) - LeetHub

---
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+
+class Solution {
+public:
+    std::vector<std::string> belowTen = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+    std::vector<std::string> belowTwenty = { "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+    std::vector<std::string> belowHundred = { "", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+
+    std::string numberToWords(int num) {
+        if (num == 0) {
+            return "Zero";
+        }
+        return helper(num);
+    }
+
+private:
+    std::string helper(int num) {
+        if (num < 10) {
+            return belowTen[num];
+        }
+        if (num < 20) {
+            return belowTwenty[num - 10];
+        }
+        if (num < 100) {
+            return belowHundred[num / 10] + (num % 10 != 0 ? " " + belowTen[num % 10] : "");
+        }
+        if (num < 1000) {
+            return belowTen[num / 100] + " Hundred" + (num % 100 != 0 ? " " + helper(num % 100) : "");
+        }
+        if (num < 1000000) {
+            return helper(num / 1000) + " Thousand" + (num % 1000 != 0 ? " " + helper(num % 1000) : "");
+        }
+        if (num < 1000000000) {
+            return helper(num / 1000000) + " Million" + (num % 1000000 != 0 ? " " + helper(num % 1000000) : "");
+        }
+        return helper(num / 1000000000) + " Billion" + (num % 1000000000 != 0 ? " " + helper(num % 1000000000) : "");
+    }
+};
