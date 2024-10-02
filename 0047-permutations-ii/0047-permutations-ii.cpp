d1c8542e43bb8fdc7fa16af13ee63a5687266a19
Abhay suresh wadkar <154788447+Abhaywadkar04@users.noreply.github.com>
2024-10-02 16:08:27 +0530

    Time: 41 ms (16.09%), Space: 11.7 MB (49.21%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,38 +1,35 @@
-class Solution {
-    void permutation(vector<int>&nums,vector<int>&s,vector<vector<int>>&ans,int frq[]){
+#include <vector>
+#include <set>
 
-        if(s.size()==nums.size()){
-            ans.push_back(s);
+class Solution {
+    void permutation(vector<int>& nums, vector<int>& s, set<vector<int>>& uniquePerms, int frq[]) {
+        if (s.size() == nums.size()) {
+            uniquePerms.insert(s);  // Add the current permutation to the set
             return;
-            
         }
 
-        for(int i=0;i<nums.size();i++){
-            if(!frq[i]){
-                s.push_back(nums[i]);
-                frq[i]=1;
-                permutation(nums,s,ans,frq);
-                frq[i]=0;
-                s.pop_back();
+        for (int i = 0; i < nums.size(); i++) {
+            if (!frq[i]) {  // If nums[i] hasn't been used in the current permutation
+                s.push_back(nums[i]);  // Add nums[i] to the current permutation
+                frq[i] = 1;  // Mark nums[i] as used
+                permutation(nums, s, uniquePerms, frq);  // Recurse to generate next elements
+                frq[i] = 0;  // Backtrack: Unmark nums[i]
+                s.pop_back();  // Remove nums[i] from the current permutation
             }
         }
-
-
     }
+
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-
-        vector<vector<int>>ans;
-        vector<int>s;
+        set<vector<int>> uniquePerms;  // To store unique permutations
+        vector<int> s;
         int frq[nums.size()];
-        for(int i=0;i<nums.size();i++)frq[i]=0;
-        permutation(nums,s,ans,frq);
-        std::sort(ans.begin(), ans.end());
+        std::fill(frq, frq + nums.size(), 0);  // Initialize frequency array to 0
 
-    auto last = std::unique(ans.begin(), ans.end());
+        permutation(nums, s, uniquePerms, frq);  // Generate permutations
 
-    ans.erase(last, ans.end());
-    return ans;
-        
+        // Convert the set to a vector of vectors
+        vector<vector<int>> ans(uniquePerms.begin(), uniquePerms.end());
+        return ans;  // Return the unique permutations
     }
 };
