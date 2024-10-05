eb41d6741a7489a27b52bdcddb30fb5655fc2329
Abhay suresh wadkar <154788447+Abhaywadkar04@users.noreply.github.com>
2024-10-05 18:11:17 +0530

    Time: 1057 ms (7.19%), Space: 60.3 MB (12.51%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,45 +1,24 @@
 class Solution {
 public:
-    bool checkInclusion(string p, string s)
-    {
-        unordered_map<char,int>m;
-        int n=s.length();
-        int k=p.length();
-        for(auto i:p)
-            m[i]++;
-        int c=m.size();
-        int i=0,j=0;
-        while(j<n)
+    bool checkInclusion(string s1, string s2) {
+        int m = s1.length();
+        int i = 0;
+        string str = s2.substr(i,m); // creating substring of the length of s1
+        // This initialization is needed to enter into the loop
+        sort(s1.begin(),s1.end()); //sort s1
+        while(str.length()==m) // break the loop when there are not 
+        // enough elements in the substring of the length of our 
+        // given s1 string
         {
-            if(m.count(s[j]))
+            str = s2.substr(i,m); //starting from initial substring of length s2
+            sort(str.begin(),str.end()); //sorting that substring
+            if(str==s1) // comparing if they are equal or not
             {
-                m[s[j]]--;
-               if(m[s[j]]==0)
-                   c--;
+                return true; // if equal, then return true
             }
-            
-            if((j-i+1) <k)
-                j++;
-            
-            else if((j-i+1) ==k)
-            {
-                if(c==0)
-                    return true;
-                
-                if(m.count(s[i]))
-                {
-                  m[s[i]]++;
-                    
-                  if (m[s[i]]==1)
-                  c++;
-                        
-                }
-                          i++;
-                          j++;
-            }
-           
+            i++; // otherwise go to the next index and again create a
+            // substring of the length of s1 and comapre
         }
-           return false;
-        
+        return false;
     }
 };
