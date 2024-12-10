237a9abf3cf029f5f026da68c9e045d928959c7a
Abhay suresh wadkar <154788447+Abhaywadkar04@users.noreply.github.com>
2024-12-10 14:36:26 +0530

    Time: 754 ms (5.49%), Space: 248.1 MB (5.04%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
     int maximumLength(string s) {
-        int n = s.size();
-        int l = 1, r = n;
+        unordered_map<string,int>mpp;
+        for(int i = 0;i<s.length();i++){
+            string str;
+            for(int j = i;j<s.length();j++){
+                str+= s[j];
+                unordered_set<char>set(str.begin(),str.end());
+                if(set.size()==1){
+                  mpp[str]++;
+                }
 
-        if (!helper(s, n, l)) return -1;
-
-        while (l + 1 < r) {
-            int mid = (l + r) / 2;
-            if (helper(s, n, mid)) l = mid;
-            else r = mid;
+            }
         }
-
-        return l;
-    }
-
-private:
-    bool helper(const string& s, int n, int x) {
-        vector<int> cnt(26, 0);
-        int p = 0;
-
-        for (int i = 0; i < n; i++) {
-            while (s[p] != s[i]) p++;
-            if (i - p + 1 >= x) cnt[s[i] - 'a']++;
-            if (cnt[s[i] - 'a'] > 2) return true;
+        int ans = -1;
+        for(auto it:mpp){
+            int x = it.first.length();
+            if(it.second>=3){
+               ans = max(ans,x);
+            }
         }
-
-        return false;
+        return ans;
     }
 };
