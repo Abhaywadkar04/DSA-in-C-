166ccabba536a60c7bca0191a4b3de52e4a9fd67
Abhay suresh wadkar <154788447+Abhaywadkar04@users.noreply.github.com>
2025-06-06 16:40:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Binary Search/binary-search.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int binarysearch(vector<int> &arr, int k) {
+        int start = 0;
+        int end = arr.size() - 1;
+        int result = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == k) {
+                result = mid;        
+                end = mid - 1;      
+            } else if (arr[mid] < k) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return result;
+    }
+};
